18e37c611bada1660d4998c95690a1f8f39cdf89
Lenglet-jeremy <lengletjeremy.pro@gmail.com>
2024-03-11 16:28:54 +0100

    Mise à jour TP Elicum

---
+++ 3.JS/3.Boucles/01.for.js
@@ -0,0 +1,15 @@
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// for (let j = 20; j > 10; j--) {
+//   console.log(j);
+// }
+
+for (let i = 0; i <= 20; i += 2) {
+  if (i === 14) {
+    continue; //Evite la condition
+    // break;
+  }
+  console.log(i);
+}
