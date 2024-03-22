a44397cfd813d5f95e8450836ae5eb30dfd4aceb
Lenglet-jeremy <lengletjeremy.pro@gmail.com>
2024-03-22 13:22:25 +0100

    Ajout TP SerieList

--- 3.JS/TP-SerieList/index.js
+++ 3.JS/TP-SerieList/index.js
@@ -1,3 +1,4 @@
+//Simulation API
 const series = [
   {
     name: "Breaking Bad",
@@ -8,28 +9,61 @@ const series = [
     seen: true,
   },
 ];
-
+// 1 Selectionner le point d'entree
 const ul = document.querySelector("ul");
 
-const li = document.createElement("li");
+// 2 Créer la méthode qui affiche un élément
+const createSerieElement = (serie, index) => {
+  console.log(serie.name);
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.classList.add("todo");
 
-const span = document.createElement("span");
-span.classList.add("todo", "done");
+  if (serie.seen) {
+    span.classList.add("done");
+  }
+  const p = document.createElement("p");
+  p.innerText = serie.name;
 
-const pBreakingBad = document.createElement("p");
-pBreakingBad.innerText = "Breaking Bad";
+  const btnEdit = document.createElement("button");
+  btnEdit.innerText = "Edit";
 
-const pTheWire = document.createElement("p");
-pTheWire.innerText = "The Wire";
+  const btnDelete = document.createElement("button");
+  btnDelete.innerText = "Delete";
+  btnDelete.classList.add("delete");
 
-const buttonEdited = document.createElement("button");
+  li.append(span, p, btnEdit, btnDelete);
 
-const buttonSupprimer = document.createElement("button");
-buttonSupprimer.classList.add("delete");
+  return li;
+};
 
-ul.append(li);
-li.append(span);
-li.append(pBreakingBad);
-li.append(pTheWire);
-li.append(buttonEdited);
-li.append(buttonSupprimer);
+// 3 Création de la méthode principale qui boucle sur le tableau de données
+const displaySeries = () => {
+  const seriesNode = series.map((serie, index) => {
+    return createSerieElement(serie, index);
+  });
+  ul.innerText = "";
+  ul.append(...seriesNode);
+};
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+let value = "";
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Empeche le rechargement par defaut
+  value = input.value;
+  console.log(value);
+  input.value = "";
+  addSerie(value);
+});
+
+const addSerie = (value) => {
+  series.push({ name: value, seen: false });
+  displaySeries();
+};
+
+// series étant un tableau, ajouter la valeur entré par l'utilisateur et seen = false dans un nouvel index
+// Dans le createSerieElement prendre en compte la gestion des doublons
+
+displaySeries();
